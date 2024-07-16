67f5d1cd421d5668a6b06c206f6d9fd16a2c779f
IgorPintoS <igorpintosantos05@gmail.com>
2024-07-15 21:31:31 -0300

    second commit

--- src/dio/Test.java
+++ src/dio/Test.java
@@ -1,9 +1,14 @@
-package dio.singleton;
+package dio;
+
+import  dio.singleton.SingletonBasico;
+import dio.singleton.SingletonHolder;
+import dio.singleton.SingletonResumido;
+import dio.strategy.*;
 
 public class Test {
     public static void main(String[] args) {
         //testes relacionados ao Design Pattern Singleton:
-
+        //Singleton
         SingletonBasico basico = SingletonBasico.getInstance();
         System.out.println(basico);
         basico = SingletonBasico.getInstance();
@@ -18,5 +23,21 @@ public class Test {
         System.out.println(holder);
         holder = SingletonHolder.getInstance();
         System.out.println(holder);
+
+        //Strategy
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
     }
 }
